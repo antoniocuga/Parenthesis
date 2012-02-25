62039ac56b8edf709b993b912569879a16ad00c4
Luis Cordova <cordoval@gmail.com>
2012-02-25 17:27:55 -0500

    failing 2

--- lib/PHPPeru/Parenthesis/Parenthesis.php
+++ lib/PHPPeru/Parenthesis/Parenthesis.php
@@ -6,48 +6,53 @@ class Parenthesis
 {
     public function parse($inputString)
     {
-        $needleArray = array(
-            array("(", ")"),
-            array("[", "]"),
-        );
-
-        /*foreach ($needleArray as $needle)
-        {
-            $openningNumber = substr_count($inputString, $needle[0]);
-            $closingNumber = substr_count($inputString, $needle[1]);
-            if ($openningNumber != $closingNumber)
-            {
-                return false;
-            }
-        }
-
-        return true;*/
-
-        // lee 1 por 1
         if(!$this->validateIsEqualCharcterOrCloseCharacter($inputString))
         {
             return false;
         }
+
+        return true;
     }
 
     public function validateIsEqualCharcterOrCloseCharacter($inputString)
     {
-
-        // ] = $character
-
-        // $queue1 = [[[]
-        // $queue2 = ()((
-
-        if ($character == $queue1[strlen($queue1)])
+        $queue1 = 0;
+        $queue2 = 0;
+        //var_export(strlen($inputString)-1);die;
+        for ($i = 0; $i <= strlen($inputString)-1; $i++)
         {
-            // add
-            $queue1 .= $character;
-        }
-        elseif ($character == $queue2[strlen($queue2)])
-        {
-            // remove $queue1
-            $queue1 = substr($queue1, 0, strlen($queue1) - 2);
+            $character = $inputString[$i];
+
+            if (!($queue1 >= 0 && $queue2 >= 0)) {
+                return false;
+            }
+
+            if ($character == '(' || $character == ')') {
+                if ($character == '(')
+                {
+                    $queue1++;
+                }
+                else
+                {
+                    $queue1--;
+                }
+            }
+            else {
+                if ($character == '[')
+                {
+                    $queue2++;
+                }
+                else
+                {
+                    $queue2--;
+                }
+            }
         }
 
+        if ($queue1 == 0 && $queue2 == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
